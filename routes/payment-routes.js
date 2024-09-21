bb0526a166771b209e4d594e74fc1f99028d605e
gityashh <yashrajput8359@gmail.com>
2024-09-21 21:19:32 +0530

    payment verification complete

--- routes/payment-routes.js
+++ routes/payment-routes.js
@@ -30,5 +30,29 @@ router.post('/create/orderId', async (req, res) => {
   }
 });
 
+router.post('/api/payment/verify', async (req, res) => {
+  const { razorpayOrderId, razorpayPaymentId, signature } = req.body;
+  const secret = process.env.RAZORPAY_KEY_SECRET
+
+  try {
+    const { validatePaymentVerification } = require('../node_modules/razorpay/dist/utils/razorpay-utils.js')
+
+    const result = validatePaymentVerification({ "order_id": razorpayOrderId, "payment_id": razorpayPaymentId }, signature, secret);
+    if (result) {
+      const payment = await Payment.findOne({ orderId: razorpayOrderId });
+      payment.paymentId = razorpayPaymentId;
+      payment.signature = signature;
+      payment.status = 'completed';
+      await payment.save();
+      res.json({ status: 'success' });
+    } else {
+      res.status(400).send('Invalid signature');
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error verifying payment');
+  }
+});
+
 
 module.exports = router;
